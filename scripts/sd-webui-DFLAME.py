6f5edb27f77c67f36d708da921940e3c2039a6bc
lihaoyun6 <lihaoyun11@gmail.com>
2023-06-04 21:22:06 +0800

    add fake result, add custom button label

--- scripts/sd-webui-DFLAME.py
+++ scripts/sd-webui-DFLAME.py
@@ -1,79 +1,15 @@
 import os
-import io
-import time
-import base64
 import platform
 import modules
 import gradio as gr
-import modules.shared as shared
 
+from PIL import Image
+from modules import shared
 from modules.shared import opts
-from modules.progress import ProgressRequest, ProgressResponse
 from modules.ui_common import update_generation_info, save_files, folder_symbol
-from modules import scripts, script_callbacks, shared, call_queue, ui_common, progress
+from modules import scripts, script_callbacks, call_queue, ui_common
 
-def show():
-    opts.hide_live_preview = False
-    return ({"visible": True, "__type__": "update"}, {"visible": True, "__type__": "update"}, {"visible": True, "__type__": "update"}, {"visible": True, "__type__": "update"}, {"visible": False, "__type__": "update"}, {"visible": False, "__type__": "update"})
-
-def hide():
-    opts.hide_live_preview = True
-    return ({"visible": False, "__type__": "update"}, {"visible": False, "__type__": "update"}, {"visible": False, "__type__": "update"}, {"visible": False, "__type__": "update"}, {"visible": True, "__type__": "update"}, {"visible": True, "__type__": "update"})
-
-def progressapi(req: ProgressRequest):
-    active = req.id_task == modules.progress.current_task
-    queued = req.id_task in modules.progress.pending_tasks
-    completed = req.id_task in modules.progress.finished_tasks
-
-    if not active:
-        return ProgressResponse(active=active, queued=queued, completed=completed, id_live_preview=-1, textinfo="In queue..." if queued else "Waiting...")
-
-    progress = 0
-
-    job_count, job_no = shared.state.job_count, shared.state.job_no
-    sampling_steps, sampling_step = shared.state.sampling_steps, shared.state.sampling_step
-
-    if job_count > 0:
-        progress += job_no / job_count
-    if sampling_steps > 0 and job_count > 0:
-        progress += 1 / job_count * sampling_step / sampling_steps
-
-    progress = min(progress, 1)
-
-    elapsed_since_start = time.time() - shared.state.time_start
-    predicted_duration = elapsed_since_start / progress if progress > 0 else None
-    eta = predicted_duration - elapsed_since_start if predicted_duration is not None else None
-
-    id_live_preview = req.id_live_preview
-    shared.state.set_current_image()
-
-    if opts.live_previews_enable and shared.state.id_live_preview != req.id_live_preview:
-        image = shared.state.current_image
-        if image is not None:
-            buffered = io.BytesIO()
-
-            if opts.live_previews_image_format == "png":
-                # using optimize for large images takes an enormous amount of time
-                if max(*image.size) <= 256:
-                    save_kwargs = {"optimize": True}
-                else:
-                    save_kwargs = {"optimize": False, "compress_level": 1}
-
-            else:
-                save_kwargs = {}
-
-            image.save(buffered, format=opts.live_previews_image_format, **save_kwargs)
-            base64_image = base64.b64encode(buffered.getvalue()).decode('ascii')
-            live_preview = f"data:image/{opts.live_previews_image_format};base64,{base64_image}"
-            if opts.hide_live_preview:
-                live_preview = f"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAACXBIWXMAAAsTAAALEwEAmpwYAAAADElEQVQImWP4//8/AAX+Av5Y8msOAAAAAElFTkSuQmCC"
-            id_live_preview = shared.state.id_live_preview
-        else:
-            live_preview = None
-    else:
-        live_preview = None
-
-    return ProgressResponse(active=active, queued=queued, completed=completed, progress=progress, eta=eta, live_preview=live_preview, id_live_preview=id_live_preview, textinfo=shared.state.textinfo)
+fake_img_path = os.path.join(scripts.basedir(),"images","result.png")
 
 def create_output_panel(tabname, outdir):
     from modules import shared
@@ -106,7 +42,10 @@ Requested path was: {f}
     with gr.Column(variant='panel', elem_id=f"{tabname}_results"):
         with gr.Group(elem_id=f"{tabname}_gallery_container") as gallery_container:
             result_gallery = gr.Gallery(label='Output', show_label=False, elem_id=f"{tabname}_gallery").style(columns=4)
-        fake_result_gallery = gr.Gallery(label='Output', visible=False, show_label=False, elem_id=f"{tabname}_fake_gallery").style(columns=4)
+        if opts.data.get("DFLAME_show_fake_image", True):
+            fake_result_gallery = gr.Gallery(label='Output', visible=False, value=[Image.open(fake_img_path)], show_label=False, elem_id=f"{tabname}_fake_gallery").style(columns=4)
+        else:
+            fake_result_gallery = gr.Gallery(label='Output', visible=False, show_label=False, elem_id=f"{tabname}_fake_gallery").style(columns=4)
 
         generation_info = None
         with gr.Column():
@@ -118,8 +57,8 @@ Requested path was: {f}
                     save_zip = gr.Button('Zip', elem_id=f'save_zip_{tabname}')
 
                 buttons = parameters_copypaste.create_buttons(["img2img", "inpaint", "extras"])
-                DFLAME_hide = gr.Button('Send to other', elem_id=f'DFLAME_hide_{tabname}')
-                DFLAME_show = gr.Button('Send to other', elem_id=f'DFLAME_show_{tabname}', visible=False)
+                DFLAME_hide = gr.Button(opts.data.get("DFLAME_button_text", "Send to DFLAME"), elem_id=f'DFLAME_hide_{tabname}')
+                DFLAME_show = gr.Button(opts.data.get("DFLAME_button_text", "Send to DFLAME"), elem_id=f'DFLAME_show_{tabname}', visible=False)
 
             open_folder_button.click(
                 fn=lambda: open_folder(shared.opts.outdir_samples or outdir),
@@ -177,13 +116,13 @@ Requested path was: {f}
                     )
 
                     DFLAME_hide.click(
-                        fn=hide,
+                        fn=lambda: ({"visible": False, "__type__": "update"}, {"visible": False, "__type__": "update"}, {"visible": False, "__type__": "update"}, {"visible": False, "__type__": "update"}, {"visible": True, "__type__": "update"}, {"visible": True, "__type__": "update"}),
                         inputs=[],
                         outputs=[gallery_container, DFLAME_hide, html_info, html_log, fake_result_gallery, DFLAME_show],
                     )
 
                     DFLAME_show.click(
-                        fn=show,
+                        fn=lambda: ({"visible": True, "__type__": "update"}, {"visible": True, "__type__": "update"}, {"visible": True, "__type__": "update"}, {"visible": True, "__type__": "update"}, {"visible": False, "__type__": "update"}, {"visible": False, "__type__": "update"}),
                         inputs=[],
                         outputs=[gallery_container, DFLAME_hide, html_info, html_log, fake_result_gallery, DFLAME_show],
                     )
@@ -218,8 +157,16 @@ class DFLAME(scripts.Script):
         return scripts.AlwaysVisible
 
 def hijack():
-    opts.hide_live_preview = False
     ui_common.create_output_panel = create_output_panel
-    progress.progressapi = progressapi
+
+def create_settings_items():
+    section = ('DFLAME', 'DFLAME')
+    opts.add_option("DFLAME_show_fake_image", shared.OptionInfo(
+        True, "Show fake result pictures in fake gallery (requires restart)", section=section
+    ))
+    opts.add_option("DFLAME_button_text", shared.OptionInfo(
+        "Send to DFLAME", "Button label (requires restart)", section=section
+    ))
 
 script_callbacks.on_before_ui(hijack)
+scripts.script_callbacks.on_ui_settings(create_settings_items)
